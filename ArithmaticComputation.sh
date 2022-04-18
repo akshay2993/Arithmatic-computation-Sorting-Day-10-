dcf84fdc492765c38f462fcba3e6f9edf70da852
akshayshaerma.2993@gmail.com <akshaysharma.2993@gmail.com>
2022-04-18 15:16:31 +0530

    Added computation 3-UC4.

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -4,9 +4,11 @@ echo "Welcome to Arithmatic Computation & Sorting Problem!"
 
 read -p "Enter three numbers: " a b c
 
-comp1=$(($a + $b * $c))
+comp1=$(($a+$b*$c))
 echo "a + b * c = $comp1"
 
-comp2=$(($a * $b + $c))
+comp2=$(($a*$b+$c))
 echo "a * b + c = $comp2"
 
+comp3=$(($c+$a/$b))
+echo "c + a / b = $comp3"
